3db4e3ad1334b27c1b2482cde3b674ebc7fb8d94
financieras <financieras@gmail.com>
2023-07-30 14:18:12 +0200

    tablero7

--- tablero/tablero7/main.py
+++ tablero/tablero7/main.py
@@ -20,10 +20,38 @@ class Jugador:
         self.puntaje = 0
 
     def __str__(self):
-        return f'{self.letra}: ({self.x}, {self.y})'
+        return f'{self.letra} → {self.puntaje}'
+    
+    def mover_hacia_comida(self, tablero):
+        movimientos = [(0, 1), (0, -1), (1, 0), (-1, 0)]    # tuplas: arriba, abajo, derecha, izquierda
+        mejores_movimientos = []
+        max_valor_comida = 0
 
-    def puntos_jugador(self):
-        return f'{self.letra}: {self.puntaje}'
+        for movimiento in movimientos:
+            dx, dy = movimiento
+            nueva_x = self.x + dx
+            nueva_y = self.y + dy
+
+            if 0 <= nueva_x < tablero.DIM and 0 <= nueva_y < tablero.DIM:
+                if tablero.tablero[nueva_x][nueva_y].isdigit():
+                    valor_comida = int(tablero.tablero[nueva_x][nueva_y])
+                    if valor_comida > max_valor_comida:
+                        max_valor_comida = valor_comida
+                        mejores_movimientos = [(dx, dy)]
+                    elif valor_comida == max_valor_comida:
+                        mejores_movimientos.append((dx, dy))
+
+        if mejores_movimientos:
+            dx, dy = random.choice(mejores_movimientos)
+            nueva_x = self.x + dx
+            nueva_y = self.y + dy
+
+            tablero.tablero[self.x][self.y] = '·'
+            self.x = nueva_x
+            self.y = nueva_y
+            tablero.tablero[self.x][self.y] = self.letra
+
+
 
 class Comida:
     def __init__(self, x, y, valor):
@@ -63,38 +91,43 @@ class Tablero:
         res = ''
         for fila in tablero_color:
             for cont, caracter in enumerate(fila):
-                if caracter in [chr(i) for i in range(65, 91)]:
-                    fila[cont] = f'{RED}{caracter}{RESET}'   # cambia el caracter letra mayúscula por ella misma con color
+                if caracter in [chr(i) for i in range(66, 91)]:
+                    fila[cont] = f'{GREEN}{caracter}{RESET}'   # cambia el caracter letra mayúscula por ella misma con color
+                elif caracter == 'A':
+                    fila[cont] = f'{RED}{caracter}{RESET}'
             res += ' '.join(fila) + '\n'
             #res += num_letras
         return res
 
     def mover_jugador(self, jugador):
-        movimientos = [(0, 1), (0, -1), (1, 0), (-1, 0)]    # tuplas: arriba, abajo, derecha, izquierda
-        libertades = [True] * 4   # Inicialmente suponemos que el jugador tiene todas las libertades de movimiento
-        for count, movimiento in enumerate(movimientos):
-            dx, dy = movimiento
-            nueva_x = jugador.x + dx
-            nueva_y = jugador.y + dy
-            if not (0 <= nueva_x < self.DIM and 0 <= nueva_y < self.DIM) or self.tablero[nueva_x][nueva_y].isalpha():
-                libertades[count] = False   # jugador ahogado en esa dirección, no puede moverse al tener pared u otro jugador
-        if any(libertades):  # da False cuando el jugador está ahogado en todas las direcciones, y entonces pasa turno
-            dx, dy = random.choice(movimientos)
-            nueva_x = jugador.x + dx
-            nueva_y = jugador.y + dy
-            while not (0 <= nueva_x < self.DIM and 0 <= nueva_y < self.DIM) or self.tablero[nueva_x][nueva_y].isalpha():
+        if jugador.letra == 'A':
+            jugador.mover_hacia_comida(self)
+        else:   # Resto del código para mover los otros jugadores de forma aleatoria
+            movimientos = [(0, 1), (0, -1), (1, 0), (-1, 0)]    # tuplas: arriba, abajo, derecha, izquierda
+            libertades = [True] * 4   # Inicialmente suponemos que el jugador tiene todas las libertades de movimiento
+            for count, movimiento in enumerate(movimientos):
+                dx, dy = movimiento
+                nueva_x = jugador.x + dx
+                nueva_y = jugador.y + dy
+                if not (0 <= nueva_x < self.DIM and 0 <= nueva_y < self.DIM) or self.tablero[nueva_x][nueva_y].isalpha():
+                    libertades[count] = False   # jugador ahogado en esa dirección, no puede moverse al tener pared u otro jugador
+            if any(libertades):  # da False cuando el jugador está ahogado en todas las direcciones, y entonces pasa turno
                 dx, dy = random.choice(movimientos)
                 nueva_x = jugador.x + dx
                 nueva_y = jugador.y + dy
-            if self.tablero[nueva_x][nueva_y].isdigit():
-                valor_comida = int(self.tablero[nueva_x][nueva_y])
-                jugador.puntaje += valor_comida
-                self.comidas = [comida for comida in self.comidas if not (comida.x == nueva_x and comida.y == nueva_y)]
+                while not (0 <= nueva_x < self.DIM and 0 <= nueva_y < self.DIM) or self.tablero[nueva_x][nueva_y].isalpha():
+                    dx, dy = random.choice(movimientos)
+                    nueva_x = jugador.x + dx
+                    nueva_y = jugador.y + dy
+                if self.tablero[nueva_x][nueva_y].isdigit():
+                    valor_comida = int(self.tablero[nueva_x][nueva_y])
+                    jugador.puntaje += valor_comida
+                    self.comidas = [comida for comida in self.comidas if not (comida.x == nueva_x and comida.y == nueva_y)]
 
-            self.tablero[jugador.x][jugador.y] = '·'
-            jugador.x = nueva_x
-            jugador.y = nueva_y
-            self.tablero[jugador.x][jugador.y] = jugador.letra
+                self.tablero[jugador.x][jugador.y] = '·'
+                jugador.x = nueva_x
+                jugador.y = nueva_y
+                self.tablero[jugador.x][jugador.y] = jugador.letra
 
 
 class Juego:
@@ -105,7 +138,7 @@ class Juego:
         jugadores_ordenados = sorted(self.tablero.jugadores, key=lambda jugador: jugador.puntaje, reverse=True)
         print("Ranking de jugadores:")
         for jugador in jugadores_ordenados:
-            print(jugador.puntos_jugador()) # Equivale a poner: print(f"{jugador.letra}: {jugador.puntaje}")
+            print(jugador)  # usa el str del jugador
 
     def jugar(self):
         print(self.tablero)
@@ -122,9 +155,10 @@ class Juego:
         print(f"¡El ganador es el jugador {ganador.letra} con {ganador.puntaje} puntos!\n")
         self.imprimir_ranking()
 
+
 if "__main__" == __name__:
     DIM = 10
-    num_jugadores = 26
-    num_comida = 74
-    partida = Juego(DIM, num_jugadores, num_comida)
+    NUM_PLAYERS = 26
+    AMOUNT_FOOD = 74
+    partida = Juego(DIM, NUM_PLAYERS, AMOUNT_FOOD)
     partida.jugar()
